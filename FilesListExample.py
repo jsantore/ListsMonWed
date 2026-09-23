02f1a7b07d874e04e30f222c75377718621b50a4
shinjitsu <shinjitsu@auxilioabalto.me>
2026-09-23 13:08:10 -0400

    working in class example

--- FilesListExample.py
+++ FilesListExample.py
@@ -3,5 +3,9 @@ games_file = open("games.txt", "r", encoding="utf-8")
 lines_in_file = games_file.readlines()
 #print(lines_in_file)
 for line in lines_in_file:
-    print(line)
-    #force to top
+    game_info= line.split("|")
+    price = float(game_info[2])
+    copies_sold = int(game_info[3])
+    revenue = price * copies_sold
+    game_name = game_info[0]
+    print(f"{game_name} earned ${revenue}")
